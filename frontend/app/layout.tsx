15b7e2f7adb828ff78a69497c832b97bb444dbd8
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-09-30 17:11:54 -0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 

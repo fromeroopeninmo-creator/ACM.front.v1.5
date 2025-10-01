eda41382271d0940a819fa7541fe441d8e7b44dc
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-01 17:08:12 -0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./context/ProtectedRoute";
-import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <AuthProvider>
           <ProtectedRoute>
-            <Header />
             {children}
           </ProtectedRoute>
         </AuthProvider>

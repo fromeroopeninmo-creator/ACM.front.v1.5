3982d872e959930b6f0e9746932ebdd465c63b4f
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 11:52:21 -0300

    Update layout.tsx

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,15 +1,10 @@
 "use client";
-
-import { AuthProvider } from "../context/AuthContext";
+import { AuthProvider } from "../../context/AuthContext";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <AuthProvider>{children}</AuthProvider>
-    </div>
-  );
+  return <AuthProvider>{children}</AuthProvider>;
 }

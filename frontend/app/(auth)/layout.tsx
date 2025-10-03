cb2b36967bb1ed3629824fbfb0340de7e40b4bfb
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 13:41:18 -0300

    Delete frontend/app/(auth) directory

--- frontend/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { AuthProvider } from "@/context/AuthContext";
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AuthProvider>{children}</AuthProvider>;
-}

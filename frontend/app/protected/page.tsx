041e67cdf4946f1a713c30ab77a57077f083329d
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 14:47:28 -0300

    Update page.tsx

--- frontend/app/protected/page.tsx
+++ frontend/app/protected/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-
 import ACMForm from "@/components/ACMForm";
-import ProtectedRoute from "@/context/ProtectedRoute";
 
 export default function ProtectedPage() {
   return (
-    <ProtectedRoute>
-      <div className="max-w-7xl mx-auto p-6">
-        <h1 className="text-3xl font-bold mb-6 text-center text-primary">
-          VMI - Valoración de Mercado Inmobiliario
-        </h1>
-        <div className="bg-white shadow-lg rounded-lg p-6">
-          <ACMForm />
-        </div>
+    <div className="max-w-7xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center text-primary">
+        VMI - Valoración de Mercado Inmobiliario
+      </h1>
+      <div className="bg-white shadow-lg rounded-lg p-6">
+        <ACMForm />
       </div>
-    </ProtectedRoute>
+    </div>
   );
 }

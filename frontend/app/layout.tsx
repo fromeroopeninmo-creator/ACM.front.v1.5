3f1fe2d3ff30145ebcdd9439f7543b8c40ddd403
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 14:37:07 -0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { AuthProvider } from "./context/AuthContext";
+import AuthProvider from "./context/AuthContext"; // ✅ ahora se importa el default
 
 const inter = Inter({ subsets: ["latin"] });
 

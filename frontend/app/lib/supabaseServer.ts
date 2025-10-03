2041c1c77e95b24d432cc8133b0acbabebf812fe
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 11:08:45 -0300

    Delete frontend/app/lib/supabaseServer.ts

--- frontend/app/lib/supabaseServer.ts
+++
@@ -1,7 +0,0 @@
-// lib/supabaseServer.ts
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-
-export const supabaseServer = () => {
-  return createServerComponentClient({ cookies });
-};

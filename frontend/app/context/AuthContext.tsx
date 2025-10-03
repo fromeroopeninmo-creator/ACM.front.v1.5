c374ed7be6ca193438763764f7b4d8a37e738a95
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 12:08:11 -0300

    Update AuthContext.tsx

--- frontend/app/context/AuthContext.tsx
+++ frontend/app/context/AuthContext.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
-
+import { supabase } from "#lib/supabaseClient";
 interface Profile {
   id: string;
   email: string;

43d315f4f790e9cc3c39f18c02503d8fe6c0a5e7
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 14:46:15 -0300

    Update layout.tsx

--- frontend/app/protected/layout.tsx
+++ frontend/app/protected/layout.tsx
@@ -12,7 +12,6 @@ export default function ProtectedLayout({
     <ProtectedRoute>
       <div className="min-h-screen bg-white">
         <Header />
-        {/* 👇 importante: usar <main>{children}</main> */}
         <main>{children}</main>
       </div>
     </ProtectedRoute>

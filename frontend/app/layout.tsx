8753142bb37988a90d456134607b157e29676091
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 10:35:12 -0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
-// app/layout.tsx
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./context/ProtectedRoute";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es">
       <body className={inter.className}>
-        <AuthProvider>
-          <ProtectedRoute>
-            {children}
-          </ProtectedRoute>
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );

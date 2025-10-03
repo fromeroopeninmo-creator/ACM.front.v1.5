368cf538c6a53c099e31a139f166f5ece670d30c
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-03 13:26:08 -0300

    Create layout.tsx

---
+++ frontend/app/protected/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import ProtectedRoute from "@/context/ProtectedRoute";
+import Header from "@/components/Header";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ProtectedRoute>
+      <div className="min-h-screen bg-white">
+        <Header />
+        {children}
+      </div>
+    </ProtectedRoute>
+  );
+}

046e5d636c3b797ed3a7e1e7c745ee620117d457
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-04 11:53:09 -0300

    Update Header.tsx

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -27,7 +27,7 @@ export default function Header() {
           alt=""
           className="object-contain"
           style={{
-            height: "80px", // ✅ mismo alto que el header
+            height: "100px", // ✅ mismo alto que el header
             width: "auto",
           }}
         />

75a876931b0d847e0310b15d22c9bf98dc870bec
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-04 11:15:00 -0300

    Update Header.tsx

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
         <img
           src="/logo-vai.png"
           alt=""
-          className="h-20 w-auto object-contain"
+          className="h-40 w-auto object-contain"
         />
       </div>
 

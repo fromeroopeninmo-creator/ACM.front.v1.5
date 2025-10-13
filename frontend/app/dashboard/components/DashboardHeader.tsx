8c2d7033c61150ac219d058b016b36dd30f592b5
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-13 12:09:38 -0300

    Update DashboardHeader.tsx

--- frontend/app/dashboard/components/DashboardHeader.tsx
+++ frontend/app/dashboard/components/DashboardHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useTheme } from "@/context/ThemeContext";
+import { useTheme } from "../../../context/ThemeContext"; // ← ruta corregida (relativa)
 
 interface HeaderProps {
   user: any;

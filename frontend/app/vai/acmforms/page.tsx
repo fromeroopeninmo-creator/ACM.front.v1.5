39f1e7e6f242b8435df91d24af447bd000b9168b
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-10-14 16:05:34 -0300

    Create page.tsx

---
+++ frontend/app/vai/acmforms/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ACMForm from "@/components/ACMForm";
+
+export default function ACMFormsPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <ACMForm />
+    </div>
+  );
+}

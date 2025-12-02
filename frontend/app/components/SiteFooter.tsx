df3190439f34682264893a45c6a8c1f2bd18470e
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2025-12-02 18:56:42 -0300

    Update SiteFooter.tsx

--- frontend/app/components/SiteFooter.tsx
+++ frontend/app/components/SiteFooter.tsx
@@ -133,42 +133,6 @@ export default function SiteFooter() {
                 <circle cx="17.4" cy="6.6" r="0.9" fill="currentColor" />
               </svg>
             </a>
-
-            {/* Facebook */}
-            <a
-              href="https://www.facebook.com/tu_cuenta"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-neutral-300 transition"
-              aria-label="Facebook"
-            >
-              <svg
-                className="h-5 w-5"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                fill="currentColor"
-              >
-                <path d="M13.5 22v-7h2.4l.4-3h-2.8V9.4c0-.9.3-1.5 1.6-1.5H16V5.2C15.7 5.2 14.9 5.1 14 5.1 11.8 5.1 10.3 6.4 10.3 9v3H8v3h2.3v7h3.2z" />
-              </svg>
-            </a>
-
-            {/* LinkedIn */}
-            <a
-              href="https://www.linkedin.com/company/tu_cuenta"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-neutral-300 transition"
-              aria-label="LinkedIn"
-            >
-              <svg
-                className="h-5 w-5"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-                fill="currentColor"
-              >
-                <path d="M4.98 3.5C4.98 4.6 4.1 5.5 3 5.5 1.9 5.5 1 4.6 1 3.5 1 2.4 1.9 1.5 3 1.5c1.1 0 1.98.9 1.98 2zM2.1 8.25h1.8V22H2.1V8.25zM8.7 8.25H10.4V9.7h.03c.24-.45.86-1.05 1.78-1.05 1.9 0 2.74 1.25 2.74 3.37V22h-1.8v-8.1c0-1.38-.03-3.16-1.93-3.16-1.93 0-2.22 1.5-2.22 3.06V22H8.7V8.25z" />
-              </svg>
-            </a>
           </div>
         </div>
       </div>

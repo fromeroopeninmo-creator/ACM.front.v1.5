cc1931de5e25e6d868a8d9f94a21c79313cfaf7e
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2026-07-09 10:56:39 -0300

    Create page.tsx

---
+++ frontend/app/dashboard/empresa/calculadora-alquileres/page.tsx
@@ -0,0 +1,9 @@
+import CalculadoraActualizacionAlquiler from "@/components/CalculadoraActualizacionAlquiler";
+
+export default function EmpresaCalculadoraAlquileresPage() {
+  return (
+    <div className="space-y-6">
+      <CalculadoraActualizacionAlquiler />
+    </div>
+  );
+}

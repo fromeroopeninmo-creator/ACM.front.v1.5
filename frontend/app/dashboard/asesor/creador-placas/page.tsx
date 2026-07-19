54ad4e83e22877324f7d5f92af3d5045f337b13a
fromeroopeninmo-creator <fromero.openinmo@gmail.com>
2026-07-19 13:31:13 -0300

    agregar creador de placas inmobiliarias

---
+++ frontend/app/dashboard/asesor/creador-placas/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import PlateEditor from "@/components/creador-placas/PlateEditor";
+
+export default function AsesorCreadorPlacasPage() {
+  return <PlateEditor mode="asesor" />;
+}
---
+++ frontend/app/dashboard/empresa/creador-placas/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import PlateEditor from "@/components/creador-placas/PlateEditor";
+
+export default function EmpresaCreadorPlacasPage() {
+  return <PlateEditor mode="empresa" />;
+}
